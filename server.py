9634ed834e6288638aabfca784cc2a6fc6e6f209
Vladimir Collak <vlad@collak.net>
2017-10-22 10:09:22 -0500

    Moved settings into settings.py file under settings module. checking for app_id from clients. fixed various bugs when clients disconnect of send strange data. implemented logging. moved loops into separate methods.

--- server.py
+++ server.py
@@ -2,6 +2,9 @@
 
 import queue
 import json
+import logging
+from settings import settings
+
 
 """
 Protocol Payload 
@@ -10,26 +13,24 @@ packet_type = "setup" | "normal"
 command = "forward" | "backward" | "left" | "right" | "stop"
 """
 
-APP_ID = "123adalsdjfhaldfjkahl234234234"
 
 def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
-    
 
     def get_client_data():
         # the input is in bytes, so decode it
         input_from_client_bytes = conn.recv(MAX_BUFFER_SIZE)
-
+        
         # MAX_BUFFER_SIZE is how big the message can be
         # this is test if it's sufficiently big
         import sys
         siz = sys.getsizeof(input_from_client_bytes)
         if  siz >= MAX_BUFFER_SIZE:
-            print("The length of input is probably too long: {}".format(siz))
+            logging.warning("The length of input is probably too long: {}".format(siz))
 
         # decode input and strip the end of line
         input_from_client = input_from_client_bytes.decode("utf8").rstrip()
-        print ("From client: {}".format(input_from_client))
-
+        logging.debug("Received from client: {}".format(input_from_client))
+        
         #start adding data to the queue and respond OK to client
         res = "ok"
         vysl = res.encode("utf8")  # encode the result string
@@ -41,15 +42,15 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
         
         try:
             input_from_client_object = json.loads(input_from_client)
+
         except:
-            print("Incorrect protocol. Unable to pars JSON") 
+            logging.warning("Incorrect protocol. Unable to parse JSON")
+            return 
 
         return input_from_client_object
-    
 
-
-    try:
-
+    def get_commands():
+            
         input_from_client = get_client_data() 
         input_from_client_object = parse_client_data(input_from_client)           
 
@@ -60,90 +61,129 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
             app_id = input_from_client_object["app_id"]
 
         except:
-            print("Incorrect protocol. Unable to extract commands")
+            logging.warning("Incorrect protocol. Unable to extract commands")
+            return
 
-        if client_type == "robot":
+        return (command, client_type, packet_type, app_id, input_from_client)  
+
+    def send_quit_to_client():
+        logging.info('Client ' + ip + ':' + port + " sent quit.")
+        res = "ok"
+        vysl = res.encode("utf8")  # encode the result string
+        conn.sendall(vysl)  # send it to client
+        conn.close()  # close connection
+        logging.info('Connection ' + ip + ':' + port + ' ended')
+
+    def robot_loop():
+        
+        logging.info(ip + ":" + port + " - robot connected")
             
-            while True:
-                    
-                if q.qsize() > 0:
-                    print("Queue size: {}".format(q.qsize()))
-
-                while not q.empty():
-                    
-                    data_from_queue = q.get()
-
-                    print ("Sending from queue: {}".format(data_from_queue))
-                    vysl =data_from_queue.encode("utf8")  # encode the result string
-                    conn.sendall(vysl)  # send it to client
-
-        elif client_type == "controller":
+        remain_in_loop = True
+        while remain_in_loop:
             
-            while command != "q":
+            if q.qsize() > 0:
+                logging.debug("Queue size: {}".format(q.qsize()))
+
+            while not q.empty():
                 
-                input_from_client = get_client_data() 
-                input_from_client_object = parse_client_data(input_from_client)           
+                data_from_queue = q.get()
+                logging.debug("Sending from queue to robot: {}".format(data_from_queue))
+                vysl = data_from_queue.encode("utf8")  # encode the result string
 
                 try:
-                    command = input_from_client_object['command']
-                    client_type = input_from_client_object["client_type"]
-                    packet_type = input_from_client_object["packet_type"]
-                    app_id = input_from_client_object["app_id"]
-                    
-                except:
-                    print("Incorrect protocol. Unable to extract commands")
-                        
-                #add to the queue
-                if packet_type != "hello":
-                    q.put(input_from_client)
+                    conn.sendall(vysl)  # send it to client
 
+                except BrokenPipeError:
+                    logging.critical("Unable to send to robot client. Broken pipe.")
+                    remain_in_loop = False
 
-        if command == "q":    
+    def controller_loop(command):
+        
+        logging.info(ip + ":" + port + " - controller connected")
 
-            print('Connection ' + ip + ':' + port + " sending OK and ending...")
-            res = "ok"
-            vysl = res.encode("utf8")  # encode the result string
-            conn.sendall(vysl)  # send it to client
-            conn.close()  # close connection
-            print('Connection ' + ip + ':' + port + " ended")
-
-                
+        while command != "quit":
             
-        print("exiting...")
+            try:
+                #get commands from the client
+                (command, client_type,packet_type, app_id, input_from_client) = get_commands()
+            
+            except:
+                logging.error("Unable to parse commands")
+                return command
+
+            if command == "quit":
+                return command
+
+            #add to the queue
+            if packet_type != "hello":
+                q.put(input_from_client)
+
+     
+    
+    try:
+
+        #get commands from client
+        (command, client_type,packet_type, app_id, input_from_client) = get_commands()
+
+        if app_id != settings.Settings.APP_ID.value:
+            logging.info("Wrong APP_ID. Unathorized client. Closing connection.")
+            send_quit_to_client()
+            return
+            
+        #loop until clients exits or fail
+        if client_type == "robot":
+            robot_loop()
+        elif client_type == "controller":
+            command = controller_loop(command)
+
+        #either client can send "quit" to exit
+        if command == "quit":    
+            send_quit_to_client()
+
+        logging.info("Exiting client {}:{}".format(ip,port))
 
     except ConnectionResetError:
-        print("Client sent reset. Exiting...")
+        #TODO - need to handle this better. if the thread exists
+        #should re-start it
+        logging.error("Client {}:{} sent reset. Exiting...".format(ip, port))
+    except:
+        logging.error("Unable to parse client message. Closing connection.")
+        send_quit_to_client()
       
 
 def start_server():
 
     import socket
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     # this is for easy starting/killing the app
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
     #no dely on TCP send
     soc.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-    print('Socket created')
-
+    logging.debug("Socket started")
+    
     try:
-
-        host = "192.168.1.143"
-        port = 12345
+        
+        host = settings.Settings.HOST.value
+        port = settings.Settings.PORT.value
 
         soc.bind((host, port))
-        print('Socket bind complete')
+        logging.info("Server UP on {}:{}".format(host,port))
+        
 
     except socket.error as msg:
+
         import sys
-        print('Bind failed. Error : ' + str(sys.exc_info()))
+        logging.critical('Socket bind failed. Critical Error : ' + str(sys.exc_info()))
+        logging.critical("Exiting...")
         sys.exit()
 
     #Start listening on socket
     soc.listen(10)
-    print('Socket now listening')
-
+    logging.info("Socket now accepting connections...")
+    
     # for handling task in separate jobs we need threading
     from threading import Thread
 
@@ -152,16 +192,24 @@ def start_server():
     while True:
         conn, addr = soc.accept()
         ip, port = str(addr[0]), str(addr[1])
-        print('Accepting connection from ' + ip + ':' + port)
+        logging.debug('Accepting connection from ' + ip + ':' + port)
+
         try:
             Thread(target=client_thread, args=(conn, ip, port)).start()
         except:
-            print("Terible error!")
+            logging.error("Unable to create client_thread.")
+            
             import traceback
             traceback.print_exc()
+
     soc.close()
 
+#set up loging
+logging.basicConfig(level = settings.Settings.LOGGING_LEVEL.value)
 
+#server will use this queue to gather commands 
+#from controllers and send to robots
 q = queue.Queue()   
+
 start_server()
 
---
+++ settings/settings.py
@@ -0,0 +1,12 @@
+from enum import Enum
+import logging
+
+class Settings(Enum):
+
+    HOST = "0.0.0.0"
+    PORT = 12345
+    APP_ID = "123adalsdjfhaldfjkahl234234234"
+    LOGGING_LEVEL = logging.INFO
+
+    
+
